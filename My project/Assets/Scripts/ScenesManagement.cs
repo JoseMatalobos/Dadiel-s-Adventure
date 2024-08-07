c20667829062f2dbdd226032f78256a1e4889fc8
Jose Matalobos <matalobos88@gmail.com>
2024-08-07 11:02:56 +0200

    Implementada la logica del MainScreen
    Se ha implementado la logica para que la pantalla inicial funcione correctamente. Se añadieron botones para empezar a jugar llevandonos a la escena de seleccion de personajes y se añadio un boton para cerrar el juego que nos pregunta si queremos cerrar el juego antes de hacerlo.

---
+++ My project/Assets/Scripts/ScenesManagement.cs
@@ -0,0 +1,42 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenesManagement : MonoBehaviour
+{
+    public GameObject askClosePanel;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        askClosePanel.SetActive(false);
+    }
+
+    public void LoadScene(string nextScene) 
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void CloseProgram() 
+    {
+        Application.Quit();
+        Debug.Log("Has cerrado el juego.");
+    }
+
+    public void OpenClosePanel() 
+    {
+        askClosePanel.SetActive(true);
+        Debug.Log("Has abierto el panel.");
+    }
+    public void CloseAskPanel() 
+    {
+        askClosePanel.SetActive(false);
+        Debug.Log("Has cerrado el panel.");
+    }
+    void Update()
+    {
+        
+    }
+}
